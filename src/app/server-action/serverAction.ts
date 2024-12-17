aee8694488b1cca621df565f3b80d08ea56a286d
Hose <rhwkd5012@naver.com>
2024-12-17 22:33:23 +0900

    ✨ feat: 페이지 재검증기능

--- src/app/server-action/serverAction.ts
+++ src/app/server-action/serverAction.ts
@@ -1,7 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-expressions
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { UserForm } from '@/types/users';
+import { schema } from '@/utils/schema';
 
 // { formData: { gender: 'MALE', age: 0, name: 'Jostar' } }
 // POST /server-action 200 in 1203ms
@@ -11,14 +13,35 @@ import { UserForm } from '@/types/users';
 // 코드가 간결해진다. 그리고 서버측에서만 실행되기때문에 보안적인 측면에서 좋음.
 // 이런 활용을 해볼 수 있겠다. submit을 하는 API를 가리고, API에 전달하는 데이터 자체는 암호화를 안하고
 // 클라이언트에서 암호화된 정보를 서버액션함수에서 복호화하면 될듯?
+
 const submitForm = async (formData: UserForm) => {
   try {
+    // 서버측 유효성 검사 실시
+    const validation = await schema.userSchema.validate(formData);
+    if (!validation) {
+      throw new Error('유효성 검사 실패');
+    }
+
     // fake a delay
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log({ formData });
+    // 해당페이지 재검증 (페이지 업데이트)
+    /**
+     * 페이지를 제거하고 다시 페이지를 생성한다.
+     * 풀라우트 캐시와 데이터 캐시가 증발한다.
+     */
+    // revalidatePath 사용에는 몇가지 종류가 존재한다.
+    // 이 방법은 주소에 해당하는 페이지의 재검증을 한다.
+    revalidatePath('/server-action');
+    // 이 방법은 경로의 모든 동적 페이지를 재검증을 한다.
+    revalidatePath('/book/[id]', 'page');
+    // 특정 레이아웃을 가지는 모든 페이지를 재검증을 한다.
+    revalidatePath('/(NoLayout)', 'layout');
+    // 모든 데이터를 재검증한다.
+    revalidatePath('/', 'layout');
 
     return {
       errors: null,
+      code: 200,
       data: 'data received and mutated',
     };
   } catch (error: unknown) {
